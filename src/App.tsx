1f029cbc0b2d956986efbd4405e64b4cc8e93c55
angelamoreno-berry <angela.moreno@berry.com.br>
2026-07-06 17:27:35 -0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,9 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ConsultingProvider } from "./contexts/ConsultingContext";
-import { ConsultingProviderV2 } from "./contexts-v2/ConsultingContextV2";
 import Index from "./pages/Index";
-import IndexV2 from "./pages/IndexV2";
 import HomePage from "./pages/HomePage";
-import HomePageV2 from "./pages/HomePageV2";
 import NotFound from "./pages/NotFound";
-
 
 const queryClient = new QueryClient();
 
@@ -21,7 +17,6 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          {/* Original routes */}
           <Route path="/" element={
             <ConsultingProvider><HomePage /></ConsultingProvider>
           } />
@@ -31,18 +26,6 @@ const App = () => (
           <Route path="/simulacao" element={
             <ConsultingProvider><Index projectType="simulation" /></ConsultingProvider>
           } />
-
-          {/* V2 isolated copy */}
-          <Route path="/versaorecomendacao" element={
-            <ConsultingProviderV2><HomePageV2 /></ConsultingProviderV2>
-          } />
-          <Route path="/versaorecomendacao/projetos" element={
-            <ConsultingProviderV2><IndexV2 projectType="real" /></ConsultingProviderV2>
-          } />
-          <Route path="/versaorecomendacao/simulacao" element={
-            <ConsultingProviderV2><IndexV2 projectType="simulation" /></ConsultingProviderV2>
-          } />
-
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
