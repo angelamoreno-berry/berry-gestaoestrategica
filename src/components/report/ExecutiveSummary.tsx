ea043ef188678606233a9ae34cf22e2dff659f77
angelamoreno-berry <angela.moreno@berry.com.br>
2026-07-08 15:30:41 -0300

    Create ExecutiveSummary.tsx

---
+++ src/components/report/ExecutiveSummary.tsx
@@ -0,0 +1,8 @@
+export default function ExecutiveSummary() {
+  return (
+    <section id="executive-summary">
+      <h2>Resumo Executivo</h2>
+      <p>Em desenvolvimento.</p>
+    </section>
+  );
+}
