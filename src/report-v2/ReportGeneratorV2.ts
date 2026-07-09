0ebb8ed0effcbe2ebc089a3afca76dbfc6eb6c52
angelamoreno-berry <angela.moreno@berry.com.br>
2026-07-09 09:04:24 -0300

    Update ReportGeneratorV2.ts

--- src/report-v2/ReportGeneratorV2.ts
+++ src/report-v2/ReportGeneratorV2.ts
@@ -1,3 +1,7 @@
-export function generateReportV2() {
+import { ReportV2Data } from "./types";
+
+export function generateReportV2(
+  data: ReportV2Data
+): string {
   throw new Error("Not implemented");
 }
