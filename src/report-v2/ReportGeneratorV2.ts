4f75ac00375320b883a1158d028675d8c97ef3ae
angelamoreno-berry <angela.moreno@berry.com.br>
2026-07-09 09:06:37 -0300

    Update ReportGeneratorV2.ts

--- src/report-v2/ReportGeneratorV2.ts
+++ src/report-v2/ReportGeneratorV2.ts
@@ -1,7 +1,28 @@
 import { ReportV2Data } from "./types";
 
+import Cover from "./sections/01Cover";
+import ExecutiveSummary from "./sections/02ExecutiveSummary";
+
 export function generateReportV2(
   data: ReportV2Data
 ): string {
-  throw new Error("Not implemented");
+  return `
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+<meta charset="UTF-8">
+<title>Plano Estratégico</title>
+</head>
+
+<body>
+
+${Cover(data)}
+
+${ExecutiveSummary(data.executive)}
+
+</body>
+
+</html>
+`;
 }
